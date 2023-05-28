3cd85f613567caf5c72f8daa7657061054a19484
Kenneth Schwoerke <kennethschwoerke@Kenneths-MacBook-Pro.local>
2023-05-28 18:03:24 -0500

    api tested with postman

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,11 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+require('./app/routes/tutorial.routes')(app)
+
 // set port, listen for requests
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
